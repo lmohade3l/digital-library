b32eefface3ea253459d065deded01beefe3dfd2
lmohade3l <mohade3.2710@gmail.com>
2025-01-09 17:24:41 +0330

    section 2 done
    redirect to book detail page and complete it

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -21,7 +21,7 @@ export default function Home() {
         }
 
         const response = await axios.get(
-          "https://get.taaghche.com/v2/everything?filters=%7B%22list%22:%5B%7B%22type%22:21,%22value%22:0%7D,%7B%22type%22:6,%22value%22:-30000%7D,%7B%22type%22:3,%22value%22:-2925%7D,%7B%22type%22:3,%22value%22:-28%7D,%7B%22type%22:50,%22value%22:0%7D%5D%7D&offset=1-0-0-16&trackingData=110160240&order=1"
+          "https://get.taaghche.com/v2/everything?filters=%7B%22list%22:%5B%7B%22type%22:21,%22value%22:0%7D,%7B%22type%22:6,%22value%22:-30000%7D,%7B%22type%22:3,%22value%22:-2925%7D,%7B%22type%22:3,%22value%22:-28%7D,%7B%22type%22:50,%22value%22:0%7D%5D%7D&offset=1-0-0-16&trackingData=110160240&order=1&sort=8"
         );
         
         setBookList(response?.data?.bookList?.books);
@@ -66,7 +66,6 @@ export default function Home() {
     };
   }, []);
 
-  // Show error alert if there's an error
   if (error) {
     return (
       <Box sx={{ display: "flex", justifyContent: "center", p: 2 }}>
@@ -85,7 +84,6 @@ export default function Home() {
     );
   }
 
-  // Always render BookList while loading to show skeletons
   if (isLoading) {
     return (
       <Box sx={{ display: "flex", justifyContent: "center" }}>
@@ -94,7 +92,6 @@ export default function Home() {
     );
   }
 
-  // Show empty message if no books and not loading
   if (!bookList?.length) {
     return (
       <Box sx={{ display: "flex", justifyContent: "center" }}>
@@ -103,7 +100,6 @@ export default function Home() {
     );
   }
 
-  // Show books if we have them
   return (
     <Box sx={{ display: "flex", justifyContent: "center" }}>
       <BookList bookList={bookList} isLoading={false} />
