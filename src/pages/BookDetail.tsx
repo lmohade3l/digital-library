974129c46b3ef364a0a48d8c14e83d1b2c548e55
lmohade3l <mohade3.2710@gmail.com>
2025-01-11 17:19:16 +0330

    update: image width in book detail

--- src/pages/BookDetail.tsx
+++ src/pages/BookDetail.tsx
@@ -64,7 +64,9 @@ const BookDetail = () => {
                 ":hover": { textDecoration: "underline" },
                 cursor: "pointer",
               }}
-            >{t("books")}</Typography>
+            >
+              {t("books")}
+            </Typography>
             <Typography
               sx={{ wordBreak: "break-word" }}
             >{` / ${book?.title}`}</Typography>
@@ -92,13 +94,15 @@ const BookDetail = () => {
                 width: isUnder700px ? "100%" : "250px",
                 minWidth: isUnder700px ? "auto" : "250px",
                 height: "fit-content",
+                display: "flex",
+                justifyContent: "center",
               }}
             >
               <img
                 src={book.coverUri}
                 alt={`${book.title} cover`}
                 style={{
-                  width: "100%",
+                  width: phone ? "80%" : "100%",
                   aspectRatio: "2/3",
                   objectFit: "cover",
                   boxShadow: `0 2px 4px 0 ${theme.palette.secondary.main}, 0 3px 10px 0 ${theme.palette.secondary.main}`,
